e5bfdb811e7b27cafc9acf8050ee22a65d4d6737
thiagofrohlich <thi.frohlich@gmail.com>
2015-12-07 23:32:51 -0200

    TipoClienteService

---
+++ cruel-ws/src/main/java/br/ufpr/cruel/service/TipoClienteService.java
@@ -0,0 +1,110 @@
+package br.ufpr.cruel.service;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import br.ufpr.cruel.domain.dao.TipoClienteDao;
+import br.ufpr.cruel.model.TipoCliente;
+import br.ufpr.cruel.service.transformer.TipoClienteTransformer;
+import br.ufpr.cruel.service.transformer.Transformer;
+
+@Path("/TipoCliente")
+public class TipoClienteService {
+
+	private TipoClienteDao dao = new TipoClienteDao();
+	private Transformer<TipoCliente, br.ufpr.cruel.domain.TipoCliente> transformer = new TipoClienteTransformer();
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<TipoCliente> findAll() {
+		dao.openCurrentSession();
+		List<br.ufpr.cruel.domain.TipoCliente> list = dao.findAll();
+		dao.closeCurrentSession();
+		
+		return transformer.transformToModel(list);
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/{id}")
+	public TipoCliente findById(@PathParam("id") Integer id) {
+		return transformer.transformToModel( find(id) );
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Path("/descricao/{descricao}")
+	public List<TipoCliente> findByDescricao(@PathParam("descricao") String descricao) {
+		dao.openCurrentSession();
+		List<br.ufpr.cruel.domain.TipoCliente> result = dao.findByDescricao("%"+descricao+"%");
+		dao.closeCurrentSession();
+		
+		return transformer.transformToModel( result );
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public void create(TipoCliente model) {
+		persistDb( transformer.transformToDomain(model) );
+	}
+	
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public void update(TipoCliente model) {
+		updateDb( transformer.transformToDomain(model) );
+	}
+	
+	@DELETE
+	@Consumes(MediaType.APPLICATION_JSON)
+	public void delete(@QueryParam("id") Integer id) {
+		deleteDb(id);
+	}
+	
+	private br.ufpr.cruel.domain.TipoCliente find(Integer id) {
+		dao.openCurrentSession();
+		br.ufpr.cruel.domain.TipoCliente tipo = dao.findById(id);
+		dao.closeCurrentSession();
+		return tipo;
+	}
+	
+	private void deleteDb(Integer id) {
+		br.ufpr.cruel.domain.TipoCliente tipoCliente = find(id);
+		dao.openCurrentSessionwithTransaction();
+		dao.delete(tipoCliente);
+		dao.getCurrentSession().flush();
+		dao.getCurrentSession().clear();
+		dao.getCurrentTransaction().commit();
+		dao.closeCurrentSession();
+	}
+
+	private void persistDb(br.ufpr.cruel.domain.TipoCliente tipoCliente) {
+		dao.openCurrentSessionwithTransaction();
+		dao.persist(tipoCliente);
+		dao.getCurrentSession().flush();
+		dao.getCurrentSession().clear();
+		dao.getCurrentTransaction().commit();
+		dao.closeCurrentSession();
+	}
+	
+	private void updateDb(br.ufpr.cruel.domain.TipoCliente tipoCliente) {
+		dao.openCurrentSessionwithTransaction();
+		dao.update(tipoCliente);
+		dao.getCurrentSession().flush();
+		dao.getCurrentSession().clear();
+		dao.getCurrentTransaction().commit();
+		dao.closeCurrentSession();
+	}
+
+}
---
+++ cruel-ws/src/main/java/br/ufpr/cruel/service/transformer/TipoClienteTransformer.java
@@ -0,0 +1,50 @@
+package br.ufpr.cruel.service.transformer;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.ufpr.cruel.model.TipoCliente;
+
+public class TipoClienteTransformer implements Transformer<TipoCliente, br.ufpr.cruel.domain.TipoCliente> {
+	
+	/* (non-Javadoc)
+	 * @see br.ufpr.cruel.service.transformer.Transformer#transformToDomain(br.ufpr.cruel.model.TipoCliente)
+	 */
+	@Override
+	public br.ufpr.cruel.domain.TipoCliente transformToDomain(TipoCliente model) {
+		br.ufpr.cruel.domain.TipoCliente domain = new br.ufpr.cruel.domain.TipoCliente();
+		if(model.getId() == null) {
+			domain.setId(model.getId());
+		}
+		domain.setDescricao(model.getDescricao());
+		domain.setValorRefeicao(new BigDecimal(model.getValorRefeicao().doubleValue()).setScale(2, BigDecimal.ROUND_HALF_UP));
+		return domain;
+	}
+
+	/* (non-Javadoc)
+	 * @see br.ufpr.cruel.service.transformer.Transformer#transformToModel(br.ufpr.cruel.domain.TipoCliente)
+	 */
+	@Override
+	public TipoCliente transformToModel(br.ufpr.cruel.domain.TipoCliente tipoCliente) {
+		TipoCliente model = new TipoCliente();
+		model.setId(tipoCliente.getId());
+		model.setDescricao(tipoCliente.getDescricao());
+		model.setValorRefeicao(new BigDecimal(tipoCliente.getValorRefeicao().doubleValue()).setScale(2, BigDecimal.ROUND_HALF_UP));
+		return model;
+	}
+	
+	/* (non-Javadoc)
+	 * @see br.ufpr.cruel.service.transformer.Transformer#transformToModel(java.util.List)
+	 */
+	@Override
+	public List<TipoCliente> transformToModel(List<br.ufpr.cruel.domain.TipoCliente> list) {
+		List<TipoCliente> models = new ArrayList<TipoCliente>();
+		for(br.ufpr.cruel.domain.TipoCliente tipo : list) {
+			models.add(transformToModel(tipo));
+		}
+		return models;
+	}
+	
+
+}
